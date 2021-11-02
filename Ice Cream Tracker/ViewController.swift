877dcee115cdc8dba4bd17be8260a3ff0dd29233
Kevin <bigairkevin@gmail.com>
2021-11-02 17:18:59 -0400

    Initial Commit

---
+++ Ice Cream Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ice Cream Tracker
+//
+//  Created by Kevin Veronneau on 11/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Ice Cream TrackerTests/Ice_Cream_TrackerTests.swift
@@ -0,0 +1,33 @@
+//
+//  Ice_Cream_TrackerTests.swift
+//  Ice Cream TrackerTests
+//
+//  Created by Kevin Veronneau on 11/2/21.
+//
+
+import XCTest
+@testable import Ice_Cream_Tracker
+
+class Ice_Cream_TrackerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
